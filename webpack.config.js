6597cc774d0a63ba6fa4c164c73098c1a488efeb
samuraikun <tazitaziawawa@gmail.com>
2018-04-14 16:25:26 +0900

    add babel and react loader in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,23 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: 'raw-loader'
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
   }
 };
